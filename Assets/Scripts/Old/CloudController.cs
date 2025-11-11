fe5691bf9c6f86ac9d2e8ff77a631346a80eda6c
VPDXlab <v.panin@xlab-game.com>
2025-11-11 16:49:32 +0300

    Clean

---
+++ Assets/Scripts/Old/CloudController.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+namespace Old
+{
+    public class CloudController : MonoBehaviour
+    {
+        [SerializeField] private Transform[] m_people;
+        [SerializeField] private float m_speed = 2;
+
+        private bool m_isMove;
+        private int m_index = -1;
+        private Vector3 m_position;
+
+        private void Start()
+        {
+            MoveNext();
+        }
+
+        public void MoveNext()
+        {
+            m_index++;
+
+            if (m_index >= m_people.Length)
+            {
+                m_index = 0;
+            }
+
+        
+            m_position = m_people[m_index].position;
+            m_position.y = transform.position.y;
+
+            m_isMove = true;
+        }
+
+        private void Update()
+        {
+            if (!m_isMove)
+            {
+                return;
+            }
+        
+            transform.position = Vector3.Lerp(transform.position, m_position, m_speed * Time.deltaTime);
+
+            if (Vector3.Distance(transform.position, m_position) < 0.1f)
+            {
+                m_isMove = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Old/PlayerController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+namespace Old
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private FreeCamera m_camera;
+        [SerializeField] private GameObject m_uiPanel;
+        [SerializeField] private CloudController m_cloudController;
+    
+        private void Update()
+        {
+            if (m_uiPanel.activeSelf)
+            {
+                return;
+            }
+        
+            m_camera.Move();
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                m_cloudController.MoveNext();
+            }
+        }
+    }
+}
